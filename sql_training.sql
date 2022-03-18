df8e2d0094d75102c1ea30a8c0feaf94456fe224
jgolik <golikasia@gmail.com>
2022-03-18 16:59:40 +0100

    initial commit

---
+++ sql_training.sql
@@ -0,0 +1,31 @@
+-- INSERT INTO public.cars(
+-- 	vin, model, type_id, color, driver_id, volume, power)
+-- 	VALUES ('55', 'ford mustang', 123,'green' , 96042709027, '2300', '280');
+
+-- select avg(power) 
+-- from cars
+-- where color='green';
+
+-- select count(*)
+-- from cars
+-- where model='Leon';
+
+-- select count(model), d.city
+-- from cars
+-- join drivers d on cars.driver_id=d.pesel 
+-- group by d.city;
+
+-- select *
+-- from cars
+-- join drivers d on cars.driver_id=d.pesel
+-- join car_types ct on cars.type_id=ct.id
+-- where name='Anna' and city='Wrocław' and type='coupe' and model='Mustang';
+
+-- select *
+-- from drivers
+-- where pesel < 92031200000;
+
+SELECT *
+FROM cars
+WHERE
+
